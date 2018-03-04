6afd9b59ee5230150879a72f19d53f4135d3ac56
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-04 14:05:54 -0600

    console log guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,38 @@
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 const word= ["Paris", "London", "Amman"]
 const blank = [];
+let guessesLeft = 10;
+let wrongLetter = [];
 
-let targetWord = word[Math.floor(Math.random() * word.length)];
+const targetWord = word[Math.floor(Math.random() * word.length)];
+
+
 
 for(let i = 0; i < targetWord.length; i++){
 {
   blank.push('_')
 }
 document.getElementById("solution").innerHTML = blank.join(" ")
-};
+document.getElementById("attempts").innerhtml = guessesLeft;
 
 document.onkeyup = function(letter) {
-    // inside here lives some
-    // code that will run every time
-    // a keyup event happens.
-  };
 
+let guess = event.key;
+  if(targetWord.indexOf(guess) > -1)
+  {
+    for(let i = 0; i < targetWord.length; i++){
+      if(targetWord[i] === guess){
+        blank[i] === guess;
+        console.log(blank)
+      }
+    }
+  }
+  else{
+    wrongLetter.push(guess);
+    guessesLeft--;
+    console.log(guessesLeft)
+    console.log(wrongLetter)
+    document.getElementById("guessedLetters").innerhtml = wrongLetter
+  }
+ } };
+// startGame();
