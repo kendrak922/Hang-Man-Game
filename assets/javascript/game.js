d25365fd46aa97ed5beb963753f26e203ffe2136
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-05 22:38:15 -0600

    reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,50 @@
 'use strict';
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-const word= ["Paris", "London", "Amman"]
+let word= ["Paris", "London", "Amman"]
 const blank = [];
 let guessesLeft = 10;
 let wrongLetter = [];
-
+let winCounter = 0;
 const targetWord = word[Math.floor(Math.random() * word.length)];
 
 
+function winLose(){
+  if(targetWord.length === winCounter){
+  document.getElementById("wins").innerHTML = +1; 
+  reset()
+  }else if(guessesLeft<=0){
+  reset()
+  }
 
-for(let i = 0; i < targetWord.length; i++){
-{
-  blank.push('_')
+  function reset(){
+    guessesLeft = 10;
+    wrongLetter = [];
+    winCounter = 0;
+    word = [];
+   }
+
 }
+
+for (let i = 0; i < targetWord.length; i++){
+blank.push('_')}
 document.getElementById("solution").innerHTML = blank.join(" ")
-document.getElementById("attempts").innerhtml = guessesLeft;
-
-document.onkeyup = function(letter) {
-
+document.onkeyup = function startGame (letter) {
 let guess = event.key;
-  if(targetWord.indexOf(guess) > -1)
-  {
-    for(let i = 0; i < targetWord.length; i++){
+if(targetWord.indexOf(guess) > -1){
+  for(let i = 0; i < targetWord.length; i++){
       if(targetWord[i] === guess){
-        blank[i] === guess;
-        console.log(blank)
+        blank[i] = guess;
+        blank.push(guess)
+        winCounter++
+        winLose()
       }
     }
   }
   else{
     wrongLetter.push(guess);
-    guessesLeft--;
-    console.log(guessesLeft)
-    console.log(wrongLetter)
-    document.getElementById("guessedLetters").innerhtml = wrongLetter
-  }
- } };
-// startGame();
+    guessesLeft --;
+    document.getElementById("attempts").innerHTML = guessesLeft
+    document.getElementById("guessedLetters").innerHTML = wrongLetter
+    winLose()
+  }};
+
