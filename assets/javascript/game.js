d335c6cb0c95351bb38ed97dced4c52bd912f332
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-21 11:37:47 -0500

    added styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,18 @@
 'use strict';
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-let word= ["Paris", "London", "Amman"]
+let word= ["Paris", "London", "Amman","Mexico City", "Berlin", "Ottawa", "Sanaa"]
 const blank = [];
 let guessesLeft = 10;
 let wrongLetter = [];
 let winCounter = 0;
-const targetWord = word[Math.floor(Math.random() * word.length)];
 
-
-function winLose(){
-  if(targetWord.length === winCounter){
-  document.getElementById("wins").innerHTML = +1; 
-  reset()
-  }else if(guessesLeft<=0){
-  reset()
-  }
-
-  function reset(){
-    guessesLeft = 10;
-    wrongLetter = [];
-    winCounter = 0;
-    word = [];
-   }
-
+function generate(){
+ let randWord = word[Math.floor(Math.random() * word.length)];
+ return randWord;
 }
+  const targetWord = generate();
 
-for (let i = 0; i < targetWord.length; i++){
+  for (let i = 0; i < targetWord.length; i++){
 blank.push('_')}
 document.getElementById("solution").innerHTML = blank.join(" ")
 document.onkeyup = function startGame (letter) {
@@ -46,5 +33,20 @@ if(targetWord.indexOf(guess) > -1){
     document.getElementById("attempts").innerHTML = guessesLeft
     document.getElementById("guessedLetters").innerHTML = wrongLetter
     winLose()
-  }};
+  }
+};
 
+function winLose(){
+  if(targetWord.length === winCounter){
+  document.getElementById("wins").innerHTML = +1; 
+  reset()
+  }else if(guessesLeft<=0){
+  reset()
+  }}
+
+  function reset(){
+    guessesLeft = 10;
+    wrongLetter = [];
+    winCounter = 0;
+    generate();
+   }
